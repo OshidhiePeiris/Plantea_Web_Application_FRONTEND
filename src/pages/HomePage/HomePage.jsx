6d53b0154e089c52b2f90cde0f806be2e1a0a7a7
OshidhiePeiris <it20019372@my.sliit.lk>
2022-10-12 22:50:17 +0530

    home page bugs

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -50,7 +50,7 @@ const HomePage = ({ match }) => {
       ) : (
         <>
           <Row>
-            {products.map((product) => (
+            {products || [].map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
